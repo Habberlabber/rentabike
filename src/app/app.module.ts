5cd1fa17e4aa1be4255dff284980b0ce1ffb67eb
Kasper Laursen <habberlabber@gmail.com>
2018-10-25 19:03:19 +0200

    Removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BikeCardComponent } from './bike-card/bike-card.component';
-import { CompleteComponent } from './complete/complete.component';
-import { CounterComponent } from './counter/counter.component';
 import { DemoData } from './demo-data';
 import { AvailabilityComponent } from './availability/availability.component';
 
@@ -12,8 +10,6 @@ import { AvailabilityComponent } from './availability/availability.component';
   declarations: [
     AppComponent,
     BikeCardComponent,
-    CompleteComponent,
-    CounterComponent,
     AvailabilityComponent
   ],
   imports: [
