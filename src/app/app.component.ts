10ade3527ec6178449aeba5e67202ae7ff21ad6a
Kasper Laursen <habberlabber@gmail.com>
2018-10-25 19:08:05 +0200

    Data source documentatin added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,10 @@ import { DemoData } from './demo-data';
 export class AppComponent {
   title = 'rentabike';
   bikesList;
+
+  // When the AppComponent is constructed we add the DemoData Class.
+  // This is where we get the Array containing the Bike Objects.
+  // The bikeList is then updated with the data form the DemoData class.
   constructor(private data:DemoData){
     this.bikesList = data.bikesList;
   }
